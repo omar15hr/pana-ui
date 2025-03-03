ebd62f58d03ef1e36e638e7a55fcd32666dde8d4
omar <omarhrn@gmail.com>
2025-03-02 22:16:56 -0300

    estructura de la app

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ function App() {
 
   return (
     <>
-      hola
+      <div className="text-white p-5">
+        <h1>Hello World</h1>
+        <p>Welcome to your Pana UI</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero non consequuntur deserunt nostrum adipisci suscipit beatae perferendis in earum nobis voluptate, aperiam ad sit optio modi ex alias neque dignissimos atque tempore saepe fuga, explicabo accusantium. Animi quas quae tenetur mollitia. Amet autem possimus tempora fugiat! Obcaecati excepturi laudantium tenetur, repudiandae maxime illum aliquam veritatis sequi harum magni cum animi! Quidem quis architecto, aspernatur sequi illum, consequatur, ea rem inventore earum quos asperiores. Dolores, totam odio. Omnis sunt placeat dolores quidem, officia, accusantium in vel, nihil impedit cupiditate provident minus!</p>
+      </div>
     </>
   )
 }
