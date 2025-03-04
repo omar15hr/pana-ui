6beb6bcb22e2d04dfe22fcb754cce3fed9a316f0
omar <omarhrn@gmail.com>
2025-03-04 12:40:48 -0300

    landing de pana ui

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,21 +1,71 @@
 import { useState } from "react";
 import { Bell, DocumentationSVG, Github, Moon, Sun } from "./Icons";
 
-export function SocialLinks() {
+const socialLinks = [
+  {
+    icon: <Github size={22} />,
+    link: "https://github.com/omar15hr/pana-ui",
+    title: "Github",
+  },
+  {
+    icon: <DocumentationSVG size={22} />,
+    link: "",
+    title: "Documentation",
+  },
+  {
+    icon: <Bell size={22} />,
+    link: "",
+    title: "Bell",
+  },
+];
 
-  const [theme, setTheme] = useState< 'light' | 'dark'>('dark');
+export function SocialLinks() {
+  const [theme, setTheme] = useState<"light" | "dark">("dark");
 
   const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
+    setTheme(theme === "light" ? "dark" : "light");
   };
 
   return (
     <div className="fixed left-4 bottom-4 text-left bg-[#09122e] p-2 px-6 rounded-full opacity-95 backdrop-blur">
-      <div className="flex flex-row gap-4 items-center">
-        <Github />
-        <DocumentationSVG />
-        <Bell />
-        <div onClick={toggleTheme}>{theme === 'dark' ? <Moon /> : <Sun />}</div>
+      {/* Enlaces sociales */}
+      {socialLinks.map(({ link, icon, title }) => (
+        <div key={title} className="group relative inline-block p-1">
+          <div className="flex flex-row items-center">
+          {title === "Github" ? (
+              <a href={link} target="_blank" rel="noopener noreferrer">
+                {icon}
+              </a>
+            ) : (
+              <button>{icon}</button>
+            )}
+          </div>
+
+          {/* Popover */}
+          <div className="absolute z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-200 mb-2 bottom-full w-32 bg-[#8bbd02] rounded-lg shadow-lg">
+            <div className="p-2 text-center">
+              <h3 className="text-sm font-semibold text-white">{title}</h3>
+            </div>
+          </div>
+        </div>
+      ))}
+
+      {/* Toggle de tema */}
+      <div className="group relative inline-block p-1">
+        <div className="flex flex-row gap-4 items-center">
+          <button onClick={toggleTheme}>
+            {theme === "light" ? <Moon size={22} /> : <Sun size={22} />}
+          </button>
+        </div>
+
+        {/* Popover */}
+        <div className="absolute z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-200 mb-2 bottom-full w-32 bg-[#8bbd02] rounded-lg shadow-lg">
+          <div className="p-2 text-center">
+            <h3 className="text-sm font-semibold text-white">
+              {theme === "light" ? "Modo Oscuro" : "Modo Claro"}
+            </h3>
+          </div>
+        </div>
       </div>
     </div>
   );
